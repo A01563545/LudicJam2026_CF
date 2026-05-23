5a034c224ff67829ec582e0eb50e47f4484a559a
Leonardo Arguello <Leonardo.arguelloloya@gmail.com>
2026-05-23 17:45:31 -0600

    feat: Playable Switch
    Feat function is playable

--- Assets/Scripts/SwitchScript.cs
+++ Assets/Scripts/SwitchScript.cs
@@ -5,13 +5,21 @@ public class SwitchScript : MonoBehaviour
 {
     public GameObject Verde;
     public GameObject Azul;
+
+    void Start()
+    {
+       Verde.SetActive(false);
+       Azul.SetActive(true);
+    }
+
+    
     private void OnTriggerStay2D(Collider2D other)
     {
         if (other.CompareTag("Player") && Keyboard.current.spaceKey.wasPressedThisFrame)
         {
             Debug.Log("El jugador tocó el Switch presionando Espacio");
-            if (Verde != null) Verde.SetActive(false);
-            if (Azul != null) Azul.SetActive(true);
+            if (Verde != null) Verde.SetActive(true);
+            if (Azul != null) Azul.SetActive(false);
         }
     }
 }
